899f012272bf0a565e8353b24e65334274414791
AlbanChatelet <alban.chatelet@edu.univ-fcomte.fr>
2024-05-24 14:09:28 +0200

    big poop

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="gros caca.js App"/>
   </div>
 </template>
 
