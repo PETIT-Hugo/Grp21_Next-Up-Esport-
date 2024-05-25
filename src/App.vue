7309a7fe1882d098876f21b174b056be40844da1
AlbanChatelet <alban.chatelet@edu.univ-fcomte.fr>
2024-05-25 17:47:48 +0200

    footer terminé

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@ import { onErrorCaptured } from 'vue'
 import { RouterLink, RouterView } from 'vue-router/auto'
 import Footer from './components/FooterPage.vue'
 import Header from './components/HeaderPage.vue'
+import DiscordLogo from './components/icons/DiscordLogo.vue'
+import InstagramLogo from './components/icons/InstagramLogo.vue'
+import TwitterLogo from './components/icons/TwitterLogo.vue'
 
 onErrorCaptured((err, instance, info) => {
   console.error('erreur : ', err, '\ninfo : ', info, '\ncomposant : ', instance)
--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,9 +1,83 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+import FlecheIcon from './icons/FlecheIcon.vue';
+import LogoNextUpFooter from './icons/LogoNextUpFooter.vue';
+import DiscordLogo from './icons/DiscordLogo.vue';
+import InstagramLogo from './icons/InstagramLogo.vue';
+import TwitterLogo from './icons/TwitterLogo.vue';
 </script>
 
+<style>
+.custom-bg {
+  background-color: #1C1A1A;
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
+.uppercase-title {
+  font-family: 'Mulish', sans-serif;
+  font-weight: 700; /* Gras */
+}
+
+.elements-title {
+  font-family: 'Roboto', sans-serif;
+  font-weight: 200; 
+  font-size: 12.5px;
+  color:#7C7C7C;
+}
+</style>
+
 <template>
-    <footer>
-        <RouterLink to="/">Footer</RouterLink>
-    </footer>
+  <footer class="custom-bg py-6 container">
+    <div class="mx-auto px-12">
+    <!-- Elements de la partie gauche de la pages -->
+      <!-- Footer Content -->
+      <div class="flex justify-start items-start space-x-8">
+        <!-- Mentions Légales -->
+        <div class="w-2/8">
+          <h3 class="text-white mb-1 uppercase-title">MENTIONS LÉGALES</h3>
+          <ul class="space-y-2">
+            <li><RouterLink to="#" class="text-white hover:text-gray-300 elements-title">Conditions générales de ventes</RouterLink></li>
+            <li><RouterLink to="#" class="text-white hover:text-gray-300 elements-title">Conditions générales d’utilisation</RouterLink></li>
+            <li><RouterLink to="#" class="text-white hover:text-gray-300 elements-title">Politique de confidentialité</RouterLink></li>
+            <li><RouterLink to="#" class="text-white hover:text-gray-300 elements-title">Cookies</RouterLink></li>
+          </ul>
+        </div>
+        <!-- Nous Contacter -->
+
+        <div class="w-2/8">
+        <h3 class="text-white mb-1 uppercase-title">NOUS CONTACTER</h3>
+        <ul class="space-y-2">
+            <li><RouterLink to="#" class="text-white hover:text-gray-300 elements-title">contact@nextup-esport.com</RouterLink></li>
+            <li class="flex items-center">
+            <TwitterLogo class="mr-2 hover:text-gray-300" /> <!-- Ajout de l'effet hover -->
+            <InstagramLogo class="mx-2 hover:text-gray-300" /> <!-- Ajout de l'effet hover -->
+            <DiscordLogo class="ml-2 hover:text-gray-300" /> <!-- Ajout de l'effet hover -->
+            </li>
+        </ul>
+        </div>
+
+        <!-- Découvrir -->
+        <div class="w-2/8">
+          <h3 class="text-white mb-1 uppercase-title">DÉCOUVRIR</h3>
+          <ul class="space-y-2">
+            <li><RouterLink to="#" class="text-white hover:text-gray-300 elements-title">À propos</RouterLink></li>
+          </ul>
+        </div>
+    </div>
+
+<!-- Elements de la partie droite de la pages -->
+    
+    
+      <div class="flex justify-end">
+        <LogoNextUpFooter />
+      </div>
+      <!-- Flèche SVG et texte de droits d'auteur -->
+      <div class="flex items-center justify-end text-sm text-white mt-4 uppercase-title">
+        <FlecheIcon/> <!-- Inclusion de la flèche SVG -->
+        <span class="ml-2">© 2024 NextUp E-sport</span>
+      </div>
+    </div>
+  </footer>
 </template>
