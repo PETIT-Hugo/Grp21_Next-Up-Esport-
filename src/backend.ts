3880def8cb89fc8dc60d92789d10544c7d5fa970
PETIT-Hugo <hugo.petit@edu.univ-fcomte.fr>
2024-05-27 11:49:44 +0200

    INSCRIPTION FONCTIONNELLE

--- src/backend.ts
+++ src/backend.ts
@@ -5,6 +5,12 @@ export const pb = new PocketBase('https://next-up-esport.petit-hugommi1.fr:443')
 
 // Copier ici les fonctions developpées en R214 | Système d'information
 
-export async function signUp(newUtilisateur) {
-    await pb.collection('maison').create();
+export async function signUp(newUser: any) {
+  try {
+    const record = await pb.collection('utilisateur').create(newUser)
+    return record
+  } catch (error) {
+    console.error("Erreur lors de l'inscription:", error)
+    throw error
+  }
 }
--- src/pages/inscription.vue
+++ src/pages/inscription.vue
@@ -2,53 +2,44 @@
 import { ref } from 'vue'
 import { signUp } from '@/backend'
 
-// Utiliser des refs pour les champs de saisie
 const pseudoRiot = ref('')
-const pseudo = ref('')
 const mail = ref('')
+const pseudoNextUp = ref('')
 const mdp = ref('')
 
-const handleSignUp = () => {
-  const newUtil = {
-    id_riot: pseudoRiot.value,
-    pseudo: pseudo.value,
-    mail: mail.value,
-    mdp: mdp.value,
-    upcoins: 0,
-    victoires: 0,
+const handleSignUp = async () => {
+  try {
+    const newUtil = {
+      id_riot: pseudoRiot.value,
+      mail: mail.value,
+      pseudo: pseudoNextUp.value,
+      mdp: mdp.value,
+    }
+    
+    await signUp(newUtil)
+  } catch (e) {
+    console.error(e)
   }
-  signUp(newUtil)
 }
-
 </script>
 
 <template>
   <h1>Inscription</h1>
-  <div>
-    <p>
-      <label>Pseudo Riot</label>
-      <input type="text" v-model="pseudoRiot" placeholder="Pseudo Riot" class="ml-5">
-    </p>
-    <p>
-      <label>Tagline</label>
-      <input type="text" id="tagline" placeholder="Tagline Riot" class="ml-5">
-    </p>
-    <p>
-      <label>Pseudo (affiché sur le site)</label>
-      <input type="text" v-model="pseudo" placeholder="Pseudo Nextup" class="ml-5">
-    </p>
-    <p>
-      <label>Mail</label>
-      <input type="email" v-model="mail" placeholder="Adresse mail" class="ml-5">
-    </p>
-    <p>
-      <label>Mot de passe</label>
-      <input type="password" v-model="mdp" placeholder="Mot de passe" class="ml-5">
-    </p>
-    <p>
-      <label>Confirmer le mot de passe</label>
-      <input type="password" id="mdp2" placeholder="Confirmer mot de passe" class="ml-5"/>
-    </p>
-  </div>
+  <p>
+    <label>Pseudo Riot</label>
+    <input type="text" placeholder="Pseudo Riot" class="ml-5" v-model="pseudoRiot">
+  </p>
+  <p>
+    <label>Adresse Mail</label>
+    <input type="email" placeholder="Adresse mail" class="ml-5" v-model="mail">
+  </p>
+  <p>
+    <label>Pseudo Next Up</label>
+    <input type="text" placeholder="Pseudo Next Up" class="ml-5" v-model="pseudoNextUp">
+  </p>
+  <p>
+    <label>Mot de passe</label>
+    <input type="password" minlength="1" placeholder="Mot de passe" class="ml-5" v-model="mdp">
+  </p>
   <button @click="handleSignUp" class="bg-slate-400">S'inscrire</button>
 </template>
