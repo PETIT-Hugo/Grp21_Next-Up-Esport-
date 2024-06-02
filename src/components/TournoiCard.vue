77665a79b59fff616e2445661a01e55098c97e39
PETIT-Hugo <hugo.petit@edu.univ-fcomte.fr>
2024-06-02 15:53:36 +0200

    ajout TournoiCard avec affichage en appelant dans le backend

---
+++ src/components/TournoiCard.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import type { TournoiRecord, TournoiResponse } from '@/pocketbase-types';
+
+
+const props = defineProps<TournoiResponse<any>>();
+console.log (`props : `, {...props})
+
+</script>
+
+<template>
+    <div class="border border-black mt-16 mb-16"></div>
+    <div class="bg-indigo-400">
+        <div>
+            <p>Nom du tournoi :</p>
+            <p class="font-extrabold"> {{ nom }}</p>
+        </div>
+        <div>
+            <p> Jeu :</p>
+            <p class="font-extrabold"> {{ jeu }}</p>
+        </div>
+
+        <div>
+            <p> Type :</p>
+            <p class="font-extrabold"> {{ type }}</p>
+        </div>
+        <div>
+            <p> Nombre de joueurs :</p>
+            <p class="font-extrabold"> {{ nb_joueurs }}</p>
+        </div>
+        <div>
+            <p> Date :</p>
+            <p class="font-extrabold"> {{ date }}</p>
+        </div>
+    </div>
+    <div class="border border-black mt-16 mb-16"></div>
+
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
+import TournoiCard from '@/components/TournoiCard.vue'
+import { getAllTournois } from '@/backend'
 //
+const tournoiListe = await getAllTournois()
 </script>
 
 <template>
   <h1 class="text-2xl">Page d'Accueil</h1>
+  <TournoiCard v-for="unTournoi in tournoiListe" :key="unTournoi.id" v-bind="unTournoi"/>
+
   
 
 </template>
