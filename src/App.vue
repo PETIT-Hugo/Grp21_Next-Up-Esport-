fed74bd0bb0e1a69bdc93d5c2702c7a72f7f14c4
AlbanChatelet <alban.chatelet@edu.univ-fcomte.fr>
2024-06-12 15:00:54 +0200

    boutique finie

--- src/App.vue
+++ src/App.vue
@@ -23,23 +23,7 @@ onErrorCaptured((err, instance, info) => {
   <component :is="isUserLoggedIn ? HeaderSitePage : HeaderPage" />
   <main>
     <nav>
-      <ul>
-        <li>
-          <RouterLink to="/" class="text-red-500 underline"> Accueil </RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/inscription" class="text-red-500 underline"> Inscription </RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/connexion" class="text-red-500 underline"> Connexion </RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/creationTournoi" class="text-red-500 underline"> Creation Tournoi </RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/api" class="text-red-500 underline"> Tests API </RouterLink>
-        </li>
-      </ul>
+      
     </nav>
     <RouterView v-slot="{ Component }">
       <Suspense>
